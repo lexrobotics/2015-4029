14ab61603b8851589fff85d03263bf947ba52dc5
Robotics <lexrobotics4029@gmail.com>
2014-12-10 15:27:20 -0500

    Reset master repo, time for mecanum

--- Autonomous/Robot - unused.c
+++
@@ -1,9 +0,0 @@
-typedef struct Robot {
-	float* location;
-	float* next_location;
-	float heading;
-	float desired_heading;
-	int* speed;
-	int pathIndex;
-	int* path;
-} Robot;
--- Common/Robot.h
+++
@@ -1,8 +0,0 @@
-typedef struct Robot {
-	float x, y, heading;
-	float desired_location[2];
-	float desired_heading;
-	int speed[2];
-	int pathIndex;
-	int path[32];
-};
