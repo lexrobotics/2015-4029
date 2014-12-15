35fd68e4ffa731d12cb9daf7fb41b16e3b065fb9
lexrobotics1 <lexrobotics@gmail.com>
2014-12-15 14:55:19 -0500

    Misc Changes

--- Common/Movement.h
+++ Common/Movement.h
@@ -1,7 +1,15 @@
 void translateRT(int speed, int angle);
 void translateXY(int fwd, int right);
 
+void resetEncoders() {
+	nMotorEncoder[motorBackLeft] = 0;
+	nMotorEncoder[motorBackRight] = 0;
+	nMotorEncoder[motorFrontLeft] = 0;
+	nMotorEncoder[motorFrontRight] = 0;
+}
 void translateRT(int speed, int angle) {
+	if(speed > 60)
+		speed = 60;
 	float angleRad = (angle + 45) * PI/180;
 	float pow1 = speed * sin(angleRad);
 	float pow2 = speed * cos(angleRad);
