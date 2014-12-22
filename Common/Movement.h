872526ceca373fab5c109e19fa6cf95b27076b97
lexrobotics1 <lexrobotics@gmail.com>
2014-12-22 15:48:41 -0500

    Added changeDetection function, made robot stop after movements

--- Common/Movement.h
+++ Common/Movement.h
@@ -50,10 +50,22 @@ int Ultrasonic(int sensor_index){
 
 }
 
-void tillSense(int speed, int angle, bool sees , int threshold, int sensor_index){
-		while((Ultrasonic(sensor_index)<threshold)==sees){
+void tillSense(int speed, int angle, bool see_now, int threshold, int sensor_index){
+		while((Ultrasonic(sensor_index)<threshold)==see_now){
 					translateRT(speed,angle);
 		}
+
+		translateRT(0, 0);
+}
+
+void changeDetection(int speed, int angle, int jumpThreshold, int sensor_index)
+	int dist = Ultrasonic(sensor_index);
+
+	while (abs(dist - Ultrasonic(sensor_index)) < jumpThreshold) {
+		translateRT(speed, angle);
+	}
+
+	translateRT(0, 0);
 }
 
 void parallel(int speed, int threshold, int sensorA, int sensorB){
